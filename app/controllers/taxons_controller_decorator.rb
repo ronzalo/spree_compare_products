261cac2afcb602fe3ccf0275a1973eb420643adf
GarPit <garik.piton@gmail.com>
2016-11-16 14:12:08 +0300

    fix controllers - use where and limit

--- app/controllers/taxons_controller_decorator.rb
+++ app/controllers/taxons_controller_decorator.rb
@@ -1,14 +1,11 @@
 Spree::TaxonsController.class_eval do
 
-  before_filter :find_compare_products,:only=>:show
-
-
-
+  before_filter :find_compare_products, only: :show
 
   protected
 
   def find_compare_products
-    @compare_products = Spree::Product.find(:all, :conditions => { :id => session[:compare_products]} ,:limit => 4)
+    @compare_products = Spree::Product.where(id: session[:compare_products]).limit(4)
   end
 
 end
